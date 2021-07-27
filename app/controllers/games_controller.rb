a8b7e3fa4e42a6ca0f5ad95f19a82bc012449859
ericka <ericka.maricel@gmail.com>
2021-07-27 17:53:48 -0400

    Ejercicio modificado

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,38 +1,31 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
-    require 'json'
-    require 'open-uri'
-  
-    def new
-        @letters = Array.new(10) { ('A'..'Z').to_a.sample }
-    end
-  
-    def score
-      @letters = params[:letters].split
-      @reply = params[:reply]
-      @check = check
-    end
-  
-    private
-  
-    def filter_arr(word)
-      word.upcase.split('').reject do |letter|
-        if @letters.include?(letter)
-          idx = @letters.index(letter)
-          @letters.delete_at(idx)
-        end
-      end
-    end
-  
-    def check
-      if filter_arr(@reply).any?
-        "Sorry but #{@reply} can't be built out of #{params[:letters]}."
-      else
-        url = "https://wagon-dictionary.herokuapp.com/#{@reply}"
-        reply_serialized = URI.open(url).read
-        english_word = JSON.parse(reply_serialized)
-        exists = english_word['found']
-        exists ? "Congratulations! #{@reply} is a valid English word!" :
-        "Sorry but #{@reply} does not seem to be a valid English word..."
-      end
-    end
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
+
+  def score
+    @word = params[:word].upcase
+    @letters = params[:letters].split
+    if in_grid?(@word, @letters) == false
+      @message = "Sorry but #{@word} can't be built out of #{@letters.each { |l| puts l }},"
+    elsif in_grid?(@word, @letters) == true && in_dict?(@word) == false
+      @message = "Sorry but #{@word} does not seem to be a valid English word..."
+    else
+      @message = "Congratulations! #{@word} is a valid English word!"
+    end
+    session[:score] = @word.length
+  end
+
+  def in_grid?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def in_dict?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
